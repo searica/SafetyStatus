c1e68d747d66b0fbad1430cc83ad3cbd7b68548c
searica <143636061+searica@users.noreply.github.com>
2023-11-10 11:39:58 -0800

    Initial commit

---
+++ Extensions/PluginExtensions.cs
@@ -0,0 +1,107 @@
+﻿// Ignore Spelling: MVBP
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace SafetyStatus.Extensions
+{
+    internal static class TypeExtensions
+    {
+        internal static List<T> GetAllPublicConstantValues<T>(this Type type)
+        {
+            return type
+                .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(T))
+                .Select(x => (T)x.GetRawConstantValue())
+                .ToList();
+        }
+
+        internal static List<T> GetAllPublicStaticValues<T>(this Type type)
+        {
+            return type
+                .GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Where(fi => fi.FieldType == typeof(T))
+                .Select(x => (T)x.GetValue(null))
+                .ToList();
+        }
+    }
+
+    internal static class GenericExtensions
+    {
+        private const BindingFlags AllBindings =
+            BindingFlags.Public
+            | BindingFlags.NonPublic
+            | BindingFlags.Instance
+            | BindingFlags.Static
+            | BindingFlags.GetField
+            | BindingFlags.SetField
+            | BindingFlags.GetProperty
+            | BindingFlags.SetProperty;
+
+        /// <summary>
+        ///     Extension for 'Object' that copies all fields from the source to the object.
+        ///     Including private and static fields.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="source">The source.</param>
+        public static void CopyFields(this object target, object source)
+        {
+            // If any this null throw an exception
+            if (target == null || source == null)
+                throw new Exception("Target or/and Source Objects are null");
+            // Getting the Types of the objects
+            Type typeTarget = source.GetType();
+            Type typeSrc = target.GetType();
+
+            // Iterate over the fields of the source instance and copy
+            // them them to their counterparts in the target instance
+            FieldInfo[] srcFields = typeSrc.GetFields(AllBindings);
+            foreach (FieldInfo srcField in srcFields)
+            {
+                FieldInfo targetField = typeTarget.GetField(srcField.Name, AllBindings);
+                if (targetField == null)
+                {
+                    continue;
+                }
+                if (!targetField.IsInitOnly)
+                {
+                    continue;
+                }
+                if (!targetField.FieldType.IsAssignableFrom(srcField.FieldType))
+                {
+                    continue;
+                }
+                // Passed all tests, lets set the value
+                targetField.SetValue(source, srcField.GetValue(target));
+            }
+        }
+
+        internal static T Ref<T>(this T o) where T : UnityEngine.Object
+        {
+            return o ? o : null;
+        }
+    }
+
+    internal static class IEnumerableExtensions
+    {
+        internal static void Dispose(this IEnumerable<IDisposable> collection)
+        {
+            foreach (IDisposable item in collection)
+            {
+                if (item != null)
+                {
+                    try
+                    {
+                        item.Dispose();
+                    }
+                    catch (Exception)
+                    {
+                        Log.LogWarning("Could not dispose of item");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SafetyStatus.cs
@@ -0,0 +1,180 @@
+﻿// Ignore Spelling: SafetyStatus Jotunn
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Jotunn.Entities;
+using Jotunn.Managers;
+using System.Reflection;
+using UnityEngine;
+
+namespace SafetyStatus
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid, Jotunn.Main.Version)]
+    internal class SafetyStatus : BaseUnityPlugin
+    {
+        internal const string Author = "Searica";
+        public const string PluginName = "SafetyStatus";
+        public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
+        public const string PluginVersion = "1.0.0";
+
+        // Use this class to add your own localization to the game
+        // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
+        //public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
+
+        internal static CustomStatusEffect SafeEffect;
+        internal const string SafeEffectName = "SafeStatusEffect";
+        internal static int SafeEffectHash;
+
+        public void Awake()
+        {
+            Log.Init(Logger);
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
+
+            Game.isModded = true;
+
+            PrefabManager.OnVanillaPrefabsAvailable += AddStatusEffect;
+        }
+
+        /// <summary>
+        ///     Create and add the Safe status effect
+        /// </summary>
+        private void AddStatusEffect()
+        {
+            try
+            {
+                StatusEffect statusEffect = ScriptableObject.CreateInstance<StatusEffect>();
+                statusEffect.name = SafeEffectName;
+                statusEffect.m_name = "Safe";
+                statusEffect.m_icon = PrefabManager.Cache.GetPrefab<CraftingStation>("piece_workbench").m_icon;
+                statusEffect.m_startMessageType = MessageHud.MessageType.TopLeft;
+                statusEffect.m_startMessage = "You feel safer";
+                statusEffect.m_stopMessageType = MessageHud.MessageType.TopLeft;
+                statusEffect.m_stopMessage = "You feel less safe";
+                SafeEffect = new CustomStatusEffect(statusEffect, false);
+                ItemManager.Instance.AddStatusEffect(SafeEffect);
+                SafeEffectHash = SafeEffect.StatusEffect.NameHash();
+            }
+            finally
+            {
+                PrefabManager.OnVanillaPrefabsAvailable -= AddStatusEffect;
+            }
+        }
+
+        [HarmonyPatch(typeof(EffectArea))]
+        internal static class EffectAreaPatch
+        {
+            /// <summary>
+            ///     Catch things that are not pieces but have a PlayerBase effect
+            /// </summary>
+            /// <param name="__instance"></param>
+            [HarmonyPostfix]
+            [HarmonyPatch(nameof(EffectArea.Awake))]
+            private static void AwakePostfix(EffectArea __instance)
+            {
+                if (__instance.m_type == EffectArea.Type.PlayerBase)
+                {
+                    __instance.m_statusEffect = SafeEffectName;
+                    __instance.m_statusEffectHash = SafeEffectHash;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Piece))]
+        internal static class PiecePatch
+        {
+            /// <summary>
+            ///     Catch pieces that mods like MVBP add a PlayerBase effect to
+            /// </summary>
+            /// <param name="__instance"></param>
+            [HarmonyPostfix]
+            [HarmonyPriority(Priority.Low)]
+            [HarmonyPatch(nameof(Piece.Awake))]
+            private static void AwakePostfix(Piece __instance)
+            {
+                AddSafeEffect(__instance?.gameObject);
+            }
+
+            /// <summary>
+            ///     Catch pieces that mods like MVBP add a PlayerBase effect to
+            /// </summary>
+            /// <param name="__instance"></param>
+            [HarmonyPostfix]
+            [HarmonyPriority(Priority.Low)]
+            [HarmonyPatch(nameof(Piece.SetCreator))]
+            private static void SetCreatorPostfix(Piece __instance)
+            {
+                AddSafeEffect(__instance?.gameObject);
+            }
+
+            /// <summary>
+            ///     Scans components in children and add SafeEffect if PlayerBase effect area is found.
+            /// </summary>
+            /// <param name="gameObject"></param>
+            private static void AddSafeEffect(GameObject gameObject)
+            {
+                if (gameObject == null) { return; }
+
+                foreach (var effectArea in gameObject.GetComponentsInChildren<EffectArea>())
+                {
+                    if (effectArea.m_type == EffectArea.Type.PlayerBase)
+                    {
+                        effectArea.m_statusEffect = SafeEffectName;
+                        effectArea.m_statusEffectHash = SafeEffectHash;
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Player))]
+        internal static class PlayerPatch
+        {
+            /// <summary>
+            ///     Patch to check if the SafeStatusEffect should be removed.
+            /// </summary>
+            /// <param name="__instance"></param>
+            [HarmonyPostfix]
+            [HarmonyPatch(nameof(Player.UpdateEnvStatusEffects))]
+            private static void UpdateEnvStatusEffectsPostFix(Player __instance)
+            {
+                var inPlayerBase = EffectArea.IsPointInsideArea(__instance.transform.position, EffectArea.Type.PlayerBase, 1f);
+                var hasSafeEffect = __instance.m_seman.HaveStatusEffect(SafeEffectName);
+                if (hasSafeEffect && !inPlayerBase)
+                {
+                    __instance.m_seman.RemoveStatusEffect(SafeEffectHash);
+                }
+                //else if (inPlayerBase && !hasSafeEffect)
+                //{
+                //    __instance.m_seman.AddStatusEffect(SafeEffectHash);
+                //}
+            }
+        }
+    }
+
+    /// <summary>
+    /// Helper class for properly logging from static contexts.
+    /// </summary>
+    internal static class Log
+    {
+        internal static ManualLogSource _logSource;
+
+        internal static void Init(ManualLogSource logSource)
+        {
+            _logSource = logSource;
+        }
+
+        internal static void LogDebug(object data) => _logSource.LogDebug(data);
+
+        internal static void LogError(object data) => _logSource.LogError(data);
+
+        internal static void LogFatal(object data) => _logSource.LogFatal(data);
+
+        internal static void LogInfo(object data) => _logSource.LogInfo(data);
+
+        internal static void LogMessage(object data) => _logSource.LogMessage(data);
+
+        internal static void LogWarning(object data) => _logSource.LogWarning(data);
+    }
+}
