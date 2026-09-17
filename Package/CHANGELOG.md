fa349d8d39fca584bf535b3fc60acc28c0a00037
searica <143636061+searica@users.noreply.github.com>
2026-09-16 18:23:33 -0700

    Build Deep North release

--- Package/CHANGELOG.md
+++ Package/CHANGELOG.md
@@ -5,6 +5,14 @@
 			<th align="center">Notes</th>
 		</tr>
 		<tr>
+			<td align="center">1.3.0</td>
+			<td align="left">
+				<ul>
+					<li>Updated for Deep North release!</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
 			<td align="center">1.2.2</td>
 			<td align="left">
 				<ul>
--- SafetyStatus.cs
+++ SafetyStatus.cs
@@ -18,7 +18,7 @@ namespace SafetyStatus
         internal const string Author = "Searica";
         public const string PluginName = "SafetyStatus";
         public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
-        public const string PluginVersion = "1.2.2";
+        public const string PluginVersion = "1.3.0";
 
         internal static CustomStatusEffect SafeEffect;
         internal const string SafeEffectName = "SafeStatusEffect";
